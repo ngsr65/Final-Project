074580818cb8851751d4bf23aa13a8da1a6aac2d
Allison Yaeger <arywd5@mail.missouri.edu>
2017-11-09 20:23:51 -0600

    implemented the lightswitch class

--- hub/hub.ino
+++ hub/hub.ino
@@ -9,6 +9,24 @@ CSN - 53
 IRQ - Unused
 **/
 
+/*Message format 
+ * message[0] - message ID
+ * message[1] - ID of the sender
+ * message[2] - ID of the Reciever
+ * message[3] - Unused/Reserved for future use
+ * message[4] -data
+*/ 
+
+/*Node ID format
+ * 0 - Hub
+ * 1-253 Lightswitches
+ * 254 - all nodes 
+ * 255 - Uninitialized lightswitch
+ */
+
+
+
+//Libraries included 
 #include  <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
@@ -23,6 +41,30 @@ uint64_t pipe = 0xF0F0F0F0E1LL;
 byte message[5];
 String typedmessage = "";
 char letter;
+
+//Lightswitch class 
+class Lightswitch{
+  private:
+    byte ID;                 //variable to hold ID number of lightswitch
+    byte currentState;      //variable to hold the current state of the light
+    String lightswitchName; //string to hold the name of the lightswitch
+
+  public:
+  //Constructors and Destructors 
+  Lightswitch();            //default constructor 
+
+  //Methods 
+  byte getID(){return ID;}
+  byte getCurrentState(){return currentState;}
+  void setID(byte id){ID = id;}
+  void setCurrentState(byte state){currentState = state;}
+};
+//Lightswitch constructor 
+Lightswitch::Lightswitch(){
+  ID = 255;             //Using reserved ID of 255 to show it has not been initialized
+  currentState = 22;   //uninitialized
+  lightswitchName = "";
+}
 
 void setup() {
   Serial.begin(9600);
